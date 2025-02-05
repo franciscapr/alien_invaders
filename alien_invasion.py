ced01f966cafd4377825138e7def474917261306
Francisca <francisca.paredes.pr@gmail.com>
2025-02-05 10:52:47 -0300

    alien_invasion.py

---
+++ alien_invasion.py
@@ -0,0 +1,40 @@
+import sys    # Usamos la herramientas de sys para salir del juego cuando el jugador quiera.
+import pygame    # Contiene las funcionalidad que necesitamos para crear un juego.
+
+# Definimos una clase AlienINvasion()
+class AlienInvasion:
+    """Clase general para gestionar los recursos y el comportamiento del juego."""
+
+    # Funciòn que inicializa la configuraciòn de fondo de pygame.
+    def __init__(self):
+        """Inicializa el juego y crea recursos."""
+        pygame.init()
+
+    # Creamos una ventana con display.set_mode (1200, 800), es una tupla que define las dimensiones de la ventana del juego 1200 pixeles de ancho por 800 de alto.
+    # Asignamos la ventana al atributo self.screen --> para que este disponible en todos los mètodo de clase
+    # Este objeto se denomina superficie
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
+
+    # Controla el juego --> contiene un bucle while
+    # El bucle while --> contiene un buble de eventos y codigo para administrar las actualizaciones de la pantalla.
+    # Un evento --> Es una accion que realiza el usuario en la pantalla
+    def run_game(self):
+        """Inicia el buble principal para el juego."""
+        while True:
+            # Busca eventos de teclas y ratòn
+            # Con pygame.event.get() --> Obtenemos los eventos detectados por pygame
+            for event in pygame.event.get():
+                # Los if los utilizamos para detectar eventos especificos.
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # Hace visible la ùltima pantalla dibujada.
+            # Esta llamada actualiza constantemente la pantalla
+            pygame.display.flip()
+
+# Creamos una instancia del juego
+if __name__ == '__main__':
+    # Hace una instancia del juego y lo ejecuta.
+    ai = AlienInvasion()
+    ai.run_game()
