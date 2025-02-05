e6ab6126731074737637f7f27b57bdb062354918
Francisca <francisca.paredes.pr@gmail.com>
2025-02-05 11:12:14 -0300

    class Settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,6 @@
 import sys    # Usamos la herramientas de sys para salir del juego cuando el jugador quiera.
 import pygame    # Contiene las funcionalidad que necesitamos para crear un juego.
+from settings import Settings    # Importamos settings desde Settings
 
 # Definimos una clase AlienINvasion()
 class AlienInvasion:
@@ -11,16 +12,13 @@ class AlienInvasion:
         pygame.init()
         # Definimos un reloj
         self.clock = pygame.time.Clock()
+        self.settings = Settings()
 
         # Creamos una ventana con display.set_mode (1200, 800), es una tupla que define las dimensiones de la ventana del juego 1200 pixeles de ancho por 800 de alto.
         # Asignamos la ventana al atributo self.screen --> para que este disponible en todos los mètodo de clase
         # Este objeto se denomina superficie
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
-
-        # Configuraciòn del color
-        self.bg_color = (230, 230, 230)
-
 
 
     # Controla el juego --> contiene un bucle while
@@ -37,7 +35,8 @@ class AlienInvasion:
                     sys.exit()
 
             # Rediguja la pantalla en cada paso por el bucle.
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
+
 
             # Hace visible la ùltima pantalla dibujada --> Esta llamada actualiza constantemente la pantalla.
             pygame.display.flip()
---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """Una clase para guardar toda la configuraciòn de Alien Invasion."""
+
+    def __init__(self):
+        """Inicializa la configuraciòn del juego."""
+        # Configuraciòn de la pantalla
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
