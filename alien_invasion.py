a1c888630945765495b18039bcb6c1ef130a8662
Francisca <francisca.paredes.pr@gmail.com>
2025-02-07 10:08:00 -0300

    full screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -16,6 +16,11 @@ class AlienInvasion:
         self.clock = pygame.time.Clock()
         self.settings = Settings()
 
+        # self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        # self.settings.screen_width = self.screen.get_rect().width
+        # self.settings.screen_height = self.screen.get_rect().height
+        # pygame.display.set_caption("Alien Invasion")
+
         # Creamos una ventana con display.set_mode (1200, 800), es una tupla que define las dimensiones de la ventana del juego 1200 pixeles de ancho por 800 de alto.
         # Asignamos la ventana al atributo self.screen --> para que este disponible en todos los mètodo de clase
         # Este objeto se denomina superficie
