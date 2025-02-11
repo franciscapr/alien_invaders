879df0561c0cc7021588ad5f688d6806c13c0851
Francisca <francisca.paredes.pr@gmail.com>
2025-02-11 16:36:56 -0300

    aumentar la puntuaciòn

--- settings.py
+++ settings.py
@@ -22,6 +22,8 @@ class Settings:
 
         # Rapidez con la que se acelera el juego
         self.speedup_scale = 1.1
+        # Lo ràpido que aumenta el valor en puntos de los aliens
+        self.score_scale = 1.5
         self.initialize_dynamic_settings()
 
         # Configuraciòn del alien
@@ -46,3 +48,6 @@ class Settings:
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
+        # print(self.alien_points)
